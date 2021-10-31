e41135ead38a36ec6260a12bae986821cfef7d88
Claudia Silver <claudia.silver@rocketmail.com>
2021-10-31 15:59:33 +0000

    final commit

---
+++ SQL_data_generation.sql
@@ -0,0 +1,11 @@
+use myLibrary;
+
+-- Task 1:
+select substring ('Please find the substring in this string', 17, 25);
+
+-- Task 2
+select abs(-25.76823), sign(-25.76823), round(-25.76823, 2);
+
+-- Task 3:
+select current_date();
+select month(current_date()) as month;
